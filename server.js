a6df4bf0dabdf49a53570196d0fb038323410f0d
jainayushee3 <Ayushee.Jain@lntinfotech.com>
2021-02-25 20:06:27 +0530

    'uff'

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/meghna-art-studio/'));
+app.use(express.static('./dist/meghna-art-studio'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/meghna-art-studio/'}),
