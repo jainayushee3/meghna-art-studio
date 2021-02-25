2f8c4068473b0d047c475943328d05b500b100ab
jainayushee3 <Ayushee.Jain@lntinfotech.com>
2021-02-25 20:04:49 +0530

    'uff'

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/meghna-art-studio/'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/meghna-art-studio/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
